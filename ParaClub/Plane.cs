f0ef7640fac1ff09eb3539c18ec075a4028db377
LucasSimoesPolvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2023-09-06 09:06:02 +0200

    Faire le mouvement

--- ParaClub/Plane.cs
+++ ParaClub/Plane.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ParaClub
 {
-    public class Avion
+    public class Plane
     {
         // Déclaration des variable
         public int _x;
@@ -25,7 +26,27 @@ namespace ParaClub
             @"        \_____|_____/   |  "
         };
 
-        
+        public Plane(ConsoleColor color)
+        {
+            _x = 0;
+            _y = 2;
+            _color = color;
+        }
 
+        public void show()
+        {
+            Console.ForegroundColor = _color;
+
+            for(int i = 0; i < view.Length; i++)
+            {
+                Console.SetCursorPosition(_y, i+_x);
+                Console.Write(view[i]);
+            }
+        }
+
+        public void update()
+        {
+            Console.MoveBufferArea(this._x, this._y, view.Length, 6, this._y++, this._x);
+        }
     }
 }
--- ParaClub/Program.cs
+++ ParaClub/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ParaClub
 {
@@ -10,7 +11,25 @@ namespace ParaClub
     {
         static void Main(string[] args)
         {
+            // Screen config
+            Console.WindowHeight = Config.SCREEN_HEIGHT;
+            Console.WindowWidth = Config.SCREEN_WIDTH;
 
+            Plane plane = new Plane(ConsoleColor.Blue);
+
+            while (true)
+            {
+                // Modifier le modèle (ce qui *est*)
+                plane.update();
+
+
+                // Modifier ce que l'on *voit*
+                Console.Clear();
+                plane.show();
+
+                // Temporiser
+                Thread.Sleep(100);
+            }
         }
     }
 }
